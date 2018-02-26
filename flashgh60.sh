0ae709d19840dd954f2fa747a9e6ddfb11c31489
Kevin Heritage <kheritage222@gmail.com>
2018-02-26 19:45:02 +0200

    Removed images and readme and hardened flash script

--- flashgh60.sh
+++ flashgh60.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-sleep 5;
-echo "Erasing";
-sudo dfu-programmer atmega32u4 erase;
-echo "Erased";
-sleep 5;
-echo "Flashing";
-sudo dfu-programmer atmega32u4 flash ./GH60Layout.hex;
-echo "Flashed";
-sleep 5;
-echo "Resetting";
-sudo dfu-programmer atmega32u4 reset;
-echo "Reset";
+HEX_FILE=$1
+
+if [ ! -f ./$HEX_FILE ]; then
+    echo $HEX_FILE "not found";
+else
+    sleep 5;
+    echo "Erasing";
+    sudo dfu-programmer atmega32u4 erase;
+    echo "Erased";
+    sleep 5;
+    echo "Flashing" $HEX_FILE;
+    sudo dfu-programmer atmega32u4 flash $HEX_FILE;
+    echo "Flashed";
+    echo "Resetting";
+    sudo dfu-programmer atmega32u4 reset;
+    echo "Reset";
+fi
